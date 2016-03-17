84a2fe6a3ccaaf51b6914336c9c22b4f612a600f
lanou3g <18209185257@163.com>
2016-03-17 09:51:33 +0800

    第二次提交

---
+++ TestGit/TestGit/CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  TestGit
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+@end
